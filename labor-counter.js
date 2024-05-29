5de737b0d53c06cf3c5fe14d990319baad101842
Zachary Cohen <zachary@computingfun.org>
2024-05-28 20:04:56 -0400

    v1.0

--- labor-counter.js
+++ labor-counter.js
@@ -9,59 +9,103 @@
 // @grant        none
 // ==/UserScript==
 
-'use strict';
+(function () {
+    'use strict';
 
-function parseTime(str) {
-    const [time, period] = str.split(' ');
-    let [hours, minutes] = time.split(':').map(Number);
+    function parseTime(str) {
+        const [time, period] = str.split(' ');
+        let [hours, minutes] = time.split(':').map(Number);
 
-    if (period === "PM" && hours !== 12) {
-        hours += 12;
-    } else if (period === "AM" && hours === 12) {
-        hours = 0;
+        if (period === "PM" && hours !== 12) {
+            hours += 12;
+        } else if (period === "AM" && hours === 12) {
+            hours = 0;
+        }
+
+        return new Date().setHours(hours, minutes, 0, 0);
     }
 
-    return new Date().setHours(hours, minutes, 0, 0);
-}
+    function parseRange(str) {
+        const [startTime, endTime] = str.split(' - ');
+        const start = parseTime(startTime);
+        const end = parseTime(endTime);
+        return { start, end };
+    }
 
-function parseRange(str) {
-    const [startTime, endTime] = str.split(' - ');
-    const start = parseTime(startTime);
-    const end = parseTime(endTime);
-    return { start, end };
-}
+    function parseRangeIntoDifference(str) {
+        const range = parseRange(str);
+        return range.end - range.start;
+    }
 
-function parseRangeIntoDifference(str) {
-    const range = parseRange(str);
-    return range.end - range.start;
-}
+    function differenceToHoursMinutes(millisecounds) {
+        const differenceInMinutes = millisecounds / 1000 / 60;
+        const hours = Math.floor(differenceInMinutes / 60);
+        const minutes = differenceInMinutes % 60;
+        return { hours, minutes };
+    }
 
-function parseRangesIntoDifference(strArray) {
-    const diffs = strArray.map(parseRangeIntoDifference);
-    return diffs.reduce((total, diff) => {
-        return total + diff;
-    }, 0);
-}
+    function hoursMinutesFormated(hours, minutes) {
+        return `${hours}h ${minutes}m`;
+    }
 
-function differenceToHoursMinutes(millisecounds) {
-    const differenceInMinutes = millisecounds / 1000 / 60;
-    const hours = Math.floor(differenceInMinutes / 60);
-    const minutes = differenceInMinutes % 60;
-    return { hours, minutes };
-}
+    function differenceToHoursMinutesFormated(millisecounds) {
+        const hm = differenceToHoursMinutes(millisecounds);
+        return hoursMinutesFormated(hm.hours, hm.minutes);
+    }
 
-function parseRangesToHourMinutesDifference(strArray) {
-    const diff = parseRangesIntoDifference(strArray);
-    return differenceToHoursMinutes(diff);
-}
+    setInterval(() => {
+        const fraRightFrame = document.getElementById("fraRightFrame");
+        if (!fraRightFrame) {
+            return;
+        }
 
-function parseRangesToHourMinutesDifferenceFormated(strArray) {
-    const hm = parseRangesToHourMinutesDifference(strArray);
-    return `${hm.hours}h ${hm.minutes}m`;
-}
+        const fraRightFrameDoc = fraRightFrame.contentDocument;
+        if (!fraRightFrameDoc) {
+            return;
+        }
 
-function ccdashlabortest (timeStr) {
-    return parseRangesToHourMinutesDifferenceFormated(timeStr);
-}
+        const fraJobMain = fraRightFrameDoc.getElementById("fraJobMain");
+        if (!fraJobMain) {
+            return;
+        }
 
-window.ccdashlabortest = ccdashlabortest;
+        const fraJobMainDoc = fraJobMain.contentDocument;
+        if (!fraJobMainDoc) {
+            return;
+        }
+
+        const boxes = fraJobMainDoc.querySelectorAll(".boxing-1");
+        if (!boxes) {
+            return;
+        }
+
+        boxes.forEach((box) => {
+            const header = box.querySelector('td');
+            if (header && header.textContent && header.textContent.includes('Service Appointments') && !header.textContent.includes('-')) {
+                const tables = Array.from(box.querySelectorAll("tr")).slice(1);
+                if (!tables) {
+                    return;
+                }
+
+                let total_diff = 0;
+                tables.forEach((row) => {
+                    const items = row.getElementsByTagName('td');
+                    if (!items) {
+                        return;
+                    }
+
+                    const time_item = items[2];
+                    if (!time_item && !time_item.innerText) {
+                        return;
+                    }
+
+                    const range = time_item.innerText;
+                    const local_diff = parseRangeIntoDifference(range);
+                    time_item.innerText += "\n" + differenceToHoursMinutesFormated(local_diff);
+                    total_diff += local_diff;
+                });
+                header.textContent += " - " + differenceToHoursMinutesFormated(total_diff);
+            }
+        });
+    }, 3000);
+})();
