dd88f2653bc69e27dbfec94154dd3b91ef804163
Zachary Cohen <zachary@computingfun.org>
2024-05-29 18:29:24 -0400

    labor-counter move for-each part out to improve readability

--- labor-counter.js
+++ labor-counter.js
@@ -53,6 +53,59 @@
         return hoursMinutesFormated(hm.hours, hm.minutes);
     }
 
+    function forEachBox(box) {
+        const header = box.querySelector('td');
+        if (!header || !header.textContent || !header.textContent.includes('Service Appointments') || header.textContent.includes('-')) {
+            return;
+        }
+
+        const tables = Array.from(box.querySelectorAll("tr")).slice(1);
+        if (!tables) {
+            return;
+        }
+
+        const installer_times = new Map();
+        let total_diff = 0;
+
+        tables.forEach((row) => {
+            const items = row.getElementsByTagName('td');
+            if (!items) {
+                return;
+            }
+
+            const time_item = items[2];
+            if (!time_item || !time_item.innerText) {
+                return;
+            }
+
+            const installer_item = items[4];
+            if (!installer_item || !installer_item.innerText) {
+                return;
+            }
+
+            const local_diff = parseRangeIntoDifference(time_item.innerText);
+            //time_item.innerText += "\t" + differenceToHoursMinutesFormated(local_diff);
+            total_diff += local_diff;
+
+            const installer = installer_item.innerText;
+            const old_installer_diff = (installer_times.has(installer)) ? installer_times.get(installer) : 0;
+            installer_times.set(installer, old_installer_diff + local_diff);
+        });
+
+        const info_btn = document.createElement('span');
+        info_btn.innerText = differenceToHoursMinutesFormated(total_diff);
+        info_btn.onclick = () => {
+            let display = '';
+            for (const [installer, time] of installer_times) {
+                display += installer + '\t - \t' + differenceToHoursMinutesFormated(time) + '\n\n';
+            }
+            alert(display);
+        };
+
+        header.textContent += " - ";
+        header.appendChild(info_btn);
+    }
+
     setInterval(() => {
         const fraRightFrame = document.getElementById("fraRightFrame");
         if (!fraRightFrame) {
@@ -79,48 +132,6 @@
             return;
         }
 
-        boxes.forEach((box) => {
-            const header = box.querySelector('td');
-            if (header && header.textContent && header.textContent.includes('Service Appointments') && !header.textContent.includes('-')) {
-                const tables = Array.from(box.querySelectorAll("tr")).slice(1);
-                if (!tables) {
-                    return;
-                }
-
-                const installer_times = new Map();
-                let total_diff = 0;
-                tables.forEach((row) => {
-                    const items = row.getElementsByTagName('td');
-                    if (!items) {
-                        return;
-                    }
-
-                    const time_item = items[2];
-                    const installer_item = items[4];
-                    if (!time_item || !time_item.innerText || !installer_item || !installer_item.innerText) {
-                        return;
-                    }
-
-                    const local_diff = parseRangeIntoDifference(time_item.innerText);
-                    //time_item.innerText += "\t" + differenceToHoursMinutesFormated(local_diff);
-                    total_diff += local_diff;
-
-                    const installer = installer_item.innerText;
-                    const old_installer_diff = (installer_times.has(installer)) ? installer_times.get(installer) : 0;
-                    installer_times.set(installer, old_installer_diff + local_diff);
-                });
-                header.textContent += " - ";
-                const info_btn = document.createElement('span');
-                info_btn.innerText = differenceToHoursMinutesFormated(total_diff);
-                header.appendChild(info_btn);
-                info_btn.onclick = () => {
-                    let display = '';
-                    for (const [installer, time] of installer_times) {
-                        display += installer + '\t - \t' + differenceToHoursMinutesFormated(time) + '\n\n';
-                    }
-                    alert(display);
-                };
-            }
-        });
+        boxes.forEach(forEachBox);
     }, 3000);
 })();
