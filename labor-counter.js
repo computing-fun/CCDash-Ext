5f149ddd1df3f655161c4622b9d50dfe614596e0
Zachary Cohen <zachary@computingfun.org>
2024-05-30 00:15:21 -0400

    Better typing (#1)
    add typing info

--- labor-counter.js
+++ labor-counter.js
@@ -1,5 +1,10 @@
 'use strict';
 
+/**
+ * 
+ * @param {string} str 
+ * @returns 
+ */
 function parseTime(str) {
     const [time, period] = str.split(' ');
     let [hours, minutes] = time.split(':').map(Number);
@@ -13,6 +18,11 @@ function parseTime(str) {
     return new Date().setHours(hours, minutes, 0, 0);
 }
 
+/**
+ * 
+ * @param {string} str 
+ * @returns 
+ */
 function parseRange(str) {
     const [startTime, endTime] = str.split(' - ');
     const start = parseTime(startTime);
@@ -20,11 +30,21 @@ function parseRange(str) {
     return { start, end };
 }
 
+/**
+ * 
+ * @param {string} str 
+ * @returns 
+ */
 function parseRangeIntoDifference(str) {
     const range = parseRange(str);
     return range.end - range.start;
 }
 
+/**
+ * 
+ * @param {number} millisecounds 
+ * @returns 
+ */
 function differenceToHoursMinutes(millisecounds) {
     const differenceInMinutes = millisecounds / 1000 / 60;
     const hours = Math.floor(differenceInMinutes / 60);
@@ -32,71 +52,29 @@ function differenceToHoursMinutes(millisecounds) {
     return { hours, minutes };
 }
 
+/**
+ * 
+ * @param {number} hours 
+ * @param {number} minutes 
+ * @returns 
+ */
 function hoursMinutesFormated(hours, minutes) {
     return `${hours}h ${minutes}m`;
 }
 
+/**
+ * 
+ * @param {number} millisecounds 
+ * @returns 
+ */
 function differenceToHoursMinutesFormated(millisecounds) {
     const hm = differenceToHoursMinutes(millisecounds);
     return hoursMinutesFormated(hm.hours, hm.minutes);
 }
 
-function forEachBox(box) {
-    const header = box.querySelector('td');
-    if (!header || !header.textContent || !header.textContent.includes('Service Appointments') || header.textContent.includes('-')) {
-        return;
-    }
-
-    const tables = Array.from(box.querySelectorAll("tr")).slice(1);
-    if (!tables) {
-        return;
-    }
-
-    const installer_times = new Map();
-    let total_diff = 0;
-
-    tables.forEach((row) => {
-        const items = row.getElementsByTagName('td');
-        if (!items) {
-            return;
-        }
-
-        const time_item = items[2];
-        if (!time_item || !time_item.innerText) {
-            return;
-        }
-
-        const installer_item = items[4];
-        if (!installer_item || !installer_item.innerText) {
-            return;
-        }
-
-        const local_diff = parseRangeIntoDifference(time_item.innerText);
-        //time_item.innerText += "\t" + differenceToHoursMinutesFormated(local_diff);
-        total_diff += local_diff;
-
-        const installer = installer_item.innerText;
-        const old_installer_diff = (installer_times.has(installer)) ? installer_times.get(installer) : 0;
-        installer_times.set(installer, old_installer_diff + local_diff);
-    });
-
-    const info_btn = document.createElement('span');
-    info_btn.innerText = differenceToHoursMinutesFormated(total_diff);
-    info_btn.onclick = () => {
-        let display = '';
-        for (const [installer, time] of installer_times) {
-            display += installer + '\t - \t' + differenceToHoursMinutesFormated(time) + '\n\n';
-        }
-        alert(display);
-    };
-
-    header.textContent += " - ";
-    header.appendChild(info_btn);
-}
-
 setInterval(() => {
     const fraRightFrame = document.getElementById("fraRightFrame");
-    if (!fraRightFrame) {
+    if (!fraRightFrame || !(fraRightFrame instanceof HTMLIFrameElement)) {
         return;
     }
 
@@ -105,6 +83,9 @@ setInterval(() => {
         return;
     }
 
+    /**
+     * @type {HTMLFrameElement}
+     */
     const fraJobMain = fraRightFrameDoc.getElementById("fraJobMain");
     if (!fraJobMain) {
         return;
@@ -115,10 +96,49 @@ setInterval(() => {
         return;
     }
 
-    const boxes = fraJobMainDoc.querySelectorAll(".boxing-1");
-    if (!boxes) {
-        return;
-    }
+    (fraJobMainDoc.querySelectorAll(".boxing-1")).forEach((box) => {
+        const header = box.querySelector('td');
+        if (!header || !header.textContent || !header.textContent.includes('Service Appointments') || header.textContent.includes('-')) {
+            return;
+        }
 
-    boxes.forEach(forEachBox);
+        const tables = Array.from(box.querySelectorAll("tr")).slice(1);
+
+        /**
+         * @type {Map<string, number>}
+         */
+        const installer_times = new Map();
+        let total_diff = 0;
+
+        tables.forEach((row) => {
+            const items = row.getElementsByTagName('td');
+
+            const time_item = items[2];
+            const installer_item = items[4];
+            if (!time_item || !installer_item) {
+                return;
+            }
+
+            const local_diff = parseRangeIntoDifference(time_item.innerText);
+            total_diff += local_diff;
+
+            const installer = installer_item.innerText;
+            const old_installer_diff = installer_times.get(installer) ?? 0;
+            installer_times.set(installer, old_installer_diff + local_diff);
+        });
+
+        let alertDisplay = '';
+        for (const [installer, time] of installer_times) {
+            alertDisplay += installer + '\t - \t' + differenceToHoursMinutesFormated(time) + '\n\n';
+        }
+
+        const info_btn = document.createElement('span');
+        info_btn.innerText = differenceToHoursMinutesFormated(total_diff);
+        info_btn.onclick = () => {
+            alert(alertDisplay);
+        };
+
+        header.textContent += " - ";
+        header.appendChild(info_btn);
+    });
 }, 3000);
