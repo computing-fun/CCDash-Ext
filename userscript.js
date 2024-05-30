b75788a06647d71529dfcced1bfeb68e95c42470
Zachary Cohen <github@computingfun.org>
2024-05-30 00:33:26 -0400

    create userscript

---
+++ userscript.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         CCDash Ext
+// @namespace    computingfun.org
+// @version      0.1
+// @description  ...
+// @author       zachary@computingfun.org
+// @match        https://dashboard.calclosets.com/*
+// @updateURL    https://your-update-url.com/path/to/your-script.user.js
+// @downloadURL  https://your-update-url.com/path/to/your-script.user.js
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+})();
