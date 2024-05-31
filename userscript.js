c27f7cff9d9bae346ea1a197d129f96765ebaaf8
Zachary Cohen <github@computingfun.org>
2024-05-31 17:35:02 -0400

    Switch jsdelivr to the @latest version to improve updating

--- userscript.js
+++ userscript.js
@@ -5,13 +5,13 @@
 // @description  ...
 // @author       zachary@computingfun.org
 // @match        https://dashboard.calclosets.com/*
-// @updateURL    https://cdn.jsdelivr.net/gh/computing-fun/ccdash-ext@master/userscript.js
-// @downloadURL  https://cdn.jsdelivr.net/gh/computing-fun/ccdash-ext@master/userscript.js
+// @updateURL    https://cdn.jsdelivr.net/gh/computing-fun/ccdash-ext@latest/userscript.js
+// @downloadURL  https://cdn.jsdelivr.net/gh/computing-fun/ccdash-ext@latest/userscript.js
 // ==/UserScript==
 
 (function () {
     'use strict';
     ["job-titles", "times-hm-inputs", "labor-counter"].forEach((name) => {
-        import("https://cdn.jsdelivr.net/gh/computing-fun/ccdash-ext@master/" + name + ".js");
+        import("https://cdn.jsdelivr.net/gh/computing-fun/ccdash-ext@latest/" + name + ".js");
     });
 })();
