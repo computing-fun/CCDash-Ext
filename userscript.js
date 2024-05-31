15f252abe5aeed49fe88810c2b45058b1f8a4487
Zachary Cohen <github@computingfun.org>
2024-05-31 17:19:40 -0400

    Add verion Master to jsdelivr to fix syncing problem

--- userscript.js
+++ userscript.js
@@ -5,13 +5,13 @@
 // @description  ...
 // @author       zachary@computingfun.org
 // @match        https://dashboard.calclosets.com/*
-// @updateURL    https://cdn.jsdelivr.net/gh/computing-fun/ccdash-ext/userscript.js
-// @downloadURL  https://cdn.jsdelivr.net/gh/computing-fun/ccdash-ext/userscript.js
+// @updateURL    https://cdn.jsdelivr.net/gh/computing-fun/ccdash-ext@master/userscript.js
+// @downloadURL  https://cdn.jsdelivr.net/gh/computing-fun/ccdash-ext@master/userscript.js
 // ==/UserScript==
 
 (function () {
     'use strict';
     ["job-titles", "times-hm-inputs", "labor-counter"].forEach((name) => {
-        import("https://cdn.jsdelivr.net/gh/computing-fun/ccdash-ext/" + name + ".js");
+        import("https://cdn.jsdelivr.net/gh/computing-fun/ccdash-ext@master/" + name + ".js");
     });
 })();
